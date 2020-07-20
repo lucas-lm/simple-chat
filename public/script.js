f2b46bfb84478c2249087b75d1f6b4ef15606a6b
Lucas Miranda <lucas-m@outlook.com>
2020-07-20 00:06:25 -0300

    added name

--- public/script.js
+++ public/script.js
@@ -1,26 +1,33 @@
 ;(function () {
   var socket = io('http://192.168.0.2:3000')
-  const chat = document.querySelector('#chat')
-  const message = document.querySelector('input[name=message]')
-  const board = document.querySelector('#messages')
+  var chat = document.querySelector('#chat')
+  var message = document.querySelector('input[name=message]')
+  var board = document.querySelector('#messages')
 
-  function renderMessage(msg) {
-    board.innerHTML += '<p>' + msg + '</p>'
+  function renderMessage(msg, from) {
+    var itsme = from === socket.id
+    var c = itsme ? '<p class="me">' : '<p>'
+    var name = itsme ? 'Eu' : from
+    board.innerHTML += c + '<strong>' + name + '</strong>: ' + msg + '</p>'
   }
 
-  socket.on('receivedMessage', renderMessage)
+  socket.on('receivedMessage', function (m) {
+    renderMessage(m.message, m.from)
+  })
+
   socket.on('previous', function (messages) {
-    for (var message of messages) {
-      renderMessage(message)
+    console.log(messages)
+    for (var m of messages) {
+      renderMessage(m.message, m.from)
     }
   })
 
   chat.addEventListener('submit', (event) => {
     event.preventDefault()
     if (message.value) {
-      renderMessage(message.value)
+      renderMessage(message.value, socket.id)
+      socket.emit('sendMessage', { message: message.value, from: socket.id })
       message.value = ''
-      socket.emit('sendMessage', message.value)
     }
   })
 })()
--- server.js
+++ server.js
@@ -22,6 +22,7 @@ const addMessage = (s) => (msg) => {
   messages.push(msg)
   if (messages.length > 3) messages.shift()
   s.broadcast.emit('receivedMessage', msg)
+  console.log(messages)
 }
 
 io.on('connection', (s) => {
